2c6f5de20e94be82891bdfbb6d38d1125d3441d5
Jorge Casas <jorgecasas@iternova.net>
2017-09-23 02:25:39 +0200

    Updated python and opencv client / server for streaming

--- scripts/stream_server_computer.py
+++ scripts/stream_server_computer.py
@@ -1,7 +1,7 @@
 # Import libraries
 import threading
-import SocketServer
-import serial
+import socketserver
+import socket
 import cv2
 import numpy as np
 import math
@@ -12,23 +12,25 @@ server_port = 8000
 image_fps = 24 
 
 
-class VideoStreamHandler(SocketServer.StreamRequestHandler):
+
+class VideoStreamHandler(socketserver.StreamRequestHandler):
  
     def handle(self):
  
-        stream_bytes = ' ' 
+        stream_bytes = b' '
 
         # stream video frames one by one
         try:
             while True:
                 stream_bytes += self.rfile.read(1024)
-                first = stream_bytes.find('\xff\xd8')
-                last = stream_bytes.find('\xff\xd9')
+
+                first = stream_bytes.find(b'\xff\xd8')
+                last = stream_bytes.find(b'\xff\xd9')
                 if first != -1 and last != -1:
                     jpg = stream_bytes[first:last+2]
                     stream_bytes = stream_bytes[last+2:]
-                    gray = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.CV_LOAD_IMAGE_GRAYSCALE)
-                    image = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.CV_LOAD_IMAGE_UNCHANGED)
+                    gray = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_GRAYSCALE)
+                    image = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
 
                     # lower half of the image
                     half_gray = gray[120:240, :]
@@ -49,15 +51,16 @@ class VideoStreamHandler(SocketServer.StreamRequestHandler):
             print( 'Connection closed on videostream thread' )
 
 
-class ThreadServer( server_ip, server_port ):
+class ThreadServer():
 
     def server_thread(host, port):
-        server = SocketServer.TCPServer((host, port), VideoStreamHandler)
+        server = socketserver.TCPServer((host, port), VideoStreamHandler)
         server.serve_forever()
 
-    print( 'Starting videostream server in ' + server_ip + ':' + server_port )
+    print( '+ Starting videostream server in ' + str( server_ip ) + ':' + str( server_port ) )
     video_thread = threading.Thread(target=server_thread( server_ip, server_port))
     video_thread.start()
+
 
 if __name__ == '__main__':
     ThreadServer( server_ip, server_port )
